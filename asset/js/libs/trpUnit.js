041908fdf2173b2857c1a28e0875865de164a9d6
Gdj <turfrain@yahoo.co.kr>
2021-11-03 11:31:37 +0900

    time counter 추가

--- asset/js/libs/trpUnit.js
+++ asset/js/libs/trpUnit.js
@@ -2,9 +2,9 @@
  * Base			: jQuery JavaScript Library v1.12.1
  * trPackage	:
  * trpUnit    : v0.1
- * release date : 2019.12.12
+ * release date : 2021.10.29
  * author		: http://turfrain.tistory.com/
- * Copyright 2019. turfrain all rights reserved.
+ * Copyright 2021. turfrain all rights reserved.
  *
  */
 
@@ -26,21 +26,21 @@
 // ====================================
   var settings = $.extend({        
       maxTri    : 9999,       // trigger
-  minTri    : 1,					
+      minTri    : 1,					
       maxTar    : 9999,       // target
-  minTar    : 1,					
+      minTar    : 1,					
       property  : "height"
   }, options),
 
-// Do the magic math
-// =================
-    changes = function(el) {
-      var $el		= $(el),
-        elw		= $el.width(),
-        width	= elw > settings.maxTri ? settings.maxTri : elw < settings.minTri ? settings.minTri : elw,
-        size	= rangeRatio(settings.minTri, settings.maxTri, width, settings.minTar, settings.maxTar );
-      $el.css( settings.property , size + 'px');		    
-    },
+  // Do the magic math
+  // =================
+  changes = function(el) {
+    var $el		= $(el),
+      elw		= $el.width(),
+      width	= elw > settings.maxTri ? settings.maxTri : elw < settings.minTri ? settings.minTri : elw,
+      size	= rangeRatio(settings.minTri, settings.maxTri, width, settings.minTar, settings.maxTar );
+    $el.css( settings.property , size + 'px');		    
+  },
 
 
     rangeRatio = function($rMin, $rMax, $rTar, $tMin, $tMax ){	
@@ -75,10 +75,10 @@
   /* 
     trpNewsRolling							: 뉴스 롤링
     @param 	  $time  : rolling Tem 롤릴 지연시간 (default:3000)
-*   @method 	next : 다음 슬라이드
-*   @method 	prev : 이전 슬라이드
-*   @method 	play : 플레이 시작 (timer start)
-*   @method 	stop : 플레이 멈춤 (timer stop)
+  * @method 	next : 다음 슬라이드
+  * @method 	prev : 이전 슬라이드
+  * @method 	play : 플레이 시작 (timer start)
+  * @method 	stop : 플레이 멈춤 (timer stop)
   */
   $.fn.trpNewsRolling = function($time) { 
     var _setInte="";
@@ -123,12 +123,127 @@
       stop: function($index) {
         stop();
       },
-
     }
 
   };
 
   
+  /* 
+    trpCounter							              : 총값 안에서 루프 카운트를 한디.
+    @param 	  $total                      : rolling Tem 롤릴 지연시간 (default:3000)
+    @param 	  $callFn($total, $index)    : 콜백 
+  * @method 	next  : 다음 
+  * @method 	nrev  : 이전 
+  * @method 	setIndex : 인덱스 위치로 가기
+  * @method 	getTC    : .total, .index 리턴
+  */
+  $.fn.trpCounter = function($total, $callFn){
+    var _total    = $total;   // 총 
+    var _index    = 0;        // 시작
+    var _countItem = new Object();
+    if( _total <= _index ){ console.log("Error") } 
+    
+    var _count    = 0;
+    function nextFn(){
+      _count += 1; 
+      if( _count >= _total ){ _count = 0; }
+      activeFn(_count);
+    }
+    function prevFn(){
+      _count -= 1; 
+      if( _count < 0 ){ _count = _total-1; }
+      activeFn(_count);
+    }
+    function activeFn($index){
+      _index = _count = $index;
+      $callFn(_index);
+    }
+    return {
+      prev: function() { prevFn(); },
+      next: function() { nextFn(); }, 
+      setIndex: function($index) { activeFn($index); },
+      getTC: function() {
+        _countItem.total = _total;
+        _countItem.index = _index;
+        return _countItem; 
+      }
+    }
+
+  }
+
+
+
+  /* 
+  *  trpTimeProgress               : 시간을 분율로 바꾸어준다. 
+  *  @param   $options             : 설정 옴션 (setInterval, )
+  *  @param   $fractionCall($per)  : call back 함수(프로그레스)
+  *  @method 	play                 : 플레이 시작     (timer start)
+  *  @method 	stop                 : 플레이 초기화   (timer stop)
+  *  @method 	pause                : 플레이 일시멈춤 (timer pause)
+  */
+  $.fn.trpTimeProgress = function($options, $fractionCall){
+    var settings = $.extend({ 
+      interval     : 5000,
+      call         : 100
+    }, $options);
+
+    var _this        = this;   // 현컨텐츠
+    var _setTimer    = "";     // 타이머
+    var _setInterval = settings.interval   //5000;   // 카운트 인터벌
+    var _setCall     = settings.call       //100;    // 율 인터벌
+    var _setProgress = 0; 
+     
+    function trpTimer_start(){
+      if(_setTimer == "") {
+        _setTimer =  setInterval(trpActive, _setCall);
+      }
+    }
+    function trpTimer_pause(){
+      console.log("trpTimer_pause");
+      if(_setTimer != null) {
+        clearInterval(_setTimer);
+        _setTimer = "";
+      }
+    }
+    function trpTimer_stop(){
+      console.log("trpTimer_stop");
+      if(_setTimer != null) {
+        clearInterval(_setTimer);
+        _setTimer = "";
+        _setProgress = 0;
+        $fractionCall(_setProgress);
+      }
+    }
+		function trpActive(){
+      _setProgress += (_setCall/_setInterval) * 100;  // 100%; 
+      if( _setProgress >= 100){
+        _setProgress = 100;
+        trpTimer_stop();	
+        trpTimer_end();
+        $fractionCall(_setProgress);
+      }
+      $fractionCall(_setProgress);
+    }
+    function trpTimer_end(){
+      $(_this).trigger("event_trpTimer_end");
+    }
+    $(_this).on("event_trpTimer_end", function(event) {
+      console.log("========= >><< end Call in");
+    });
+
+    return {
+      play: function() {
+        trpTimer_start();
+      }, 
+      pause: function() {
+        trpTimer_pause();
+      }, 
+      stop: function() {
+        trpTimer_stop();
+      }
+    }
+
+  }
   
   
   
